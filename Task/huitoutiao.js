08957ce5841508bdea7cc2b57379373a14fac037
xiaokxiansheng <31557344+xiaokxiansheng@users.noreply.github.com>
2021-01-21 16:01:50 +0800

    Update huitoutiao.js

--- Task/huitoutiao.js
+++ Task/huitoutiao.js
@@ -41,7 +41,7 @@ function GetCookie() {
 		  $.msg(jsname, `获取url: 成功🎉`, ``);
 		  $.msg(jsname, `获取header: 成功🎉`, ``);
 		  $.msg(jsname, `获取body: 成功🎉`, ``);
-		  //$.log( jsname+"获取更新body: 成功,url:"+url);
+		  $.log(jsname+"获取更新body: 成功,url:"+url);
 	          //$.log(jsname+"获取时长url: 成功,headers:"+headers");
 		 // $.log(jsname+"获取更新body: 成功,body:"+body);
 	}
